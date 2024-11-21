9c89a92f59798a040bf47818d85aaa65fb771b52
Octavio Gauna <gaunavictoroctavio@gmail.com>
2024-11-20 21:31:27 -0300

    mostar y watch vue

--- src/ui/views/private/DashboardView.vue
+++ src/ui/views/private/DashboardView.vue
@@ -1,8 +1,15 @@
 <template>
-    <h1>Dashboard</h1>
+    <div>
+        <h1>Dashboard</h1>
+        <h3>Hola, {{ registerForm.nickname }}</h3>
+        <h3>{{ registerForm.email }}</h3>
+    </div>
 </template>
 
 <script setup lang="ts">
+    import { useRegisterStore } from '../../modules/register/stores/registerStore';
+
+    const { registerForm } = useRegisterStore();
 </script>
 
 <style scoped>
